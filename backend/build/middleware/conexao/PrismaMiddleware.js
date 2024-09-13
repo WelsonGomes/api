7cba711b19896e69127a039c6175c39e6c3e2031
Welson Gomes <welsondouglasgomes@gmail.com>
2024-09-13 19:45:12 -0300

    Atualização

--- backend/build/middleware/conexao/PrismaMiddleware.js
+++ backend/build/middleware/conexao/PrismaMiddleware.js
@@ -8,11 +8,14 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const Conn_1 = require("./connec/Conn");
 dotenv_1.default.config();
 function prismaMiddleware(req, res, next) {
+    console.log('verificando cliente na requisição');
     const schema = req.query.schema;
+    console.log('Cliente' + schema);
     if (!schema) {
-        res.status(400).json({ error: 'Cliente não definido...' });
-        return;
+        return res.status(400).json({ error: 'Cliente não definido...' });
     }
+    ;
+    console.log('Chamando comunicação do prisma.');
     req.prisma = (0, Conn_1.getPrismaClient)(schema);
     next();
 }
--- backend/build/middleware/conexao/connec/Conn.js
+++ backend/build/middleware/conexao/connec/Conn.js
@@ -9,12 +9,14 @@ const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 //Configurar cliente no banco de dados para conexão
 function getPrismaClient(schema) {
+    console.log('Configurando conexão do prisma');
     const dbhost = process.env.DATABASE_HOST;
     const dbname = process.env.DATABASE_NAME;
     const dbport = process.env.DATABASE_PORT;
     const dbuser = process.env.DATABASE_USER;
     const dbpassword = process.env.DATABASE_PASSWORD;
     const url = `postgresql://${dbuser}:${dbpassword}@${dbhost}:${dbport}/${dbname}?schema=${schema}`;
+    console.log('Cliente para comunicação ' + schema);
     return new client_1.PrismaClient({
         datasources: {
             db: {
